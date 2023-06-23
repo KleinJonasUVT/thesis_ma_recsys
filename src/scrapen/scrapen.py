2205494770c1d56b71e440797c37dc6309965a29
Jonas Klein <jonasklein2001@gmail.com>
2023-06-23 23:29:31 +0200

    updated scraping script

--- src/scrapen/scrapen.py
+++ src/scrapen/scrapen.py
@@ -10,6 +10,7 @@ import time
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import csv
+import math
 
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
@@ -25,33 +26,31 @@ driver.get(url)
 
 time.sleep(5)
 
-# Check how many courses are on this course list page
-courses = driver.find_elements(By.TAG_NAME, 'osi-course-item')
-courses_per_page = len(courses)
-
-print(courses_per_page)
-
 osi_language = driver.find_elements(By.CLASS_NAME, 'osi-language')[0]
 actions = ActionChains(driver)
 actions.move_to_element(osi_language).click().perform()
 
 time.sleep(5)
 
+# Check how many courses are on this course list page
+courses = driver.find_elements(By.TAG_NAME, 'osi-course-item')
+courses_per_page = len(courses)
+
 i = 0
 while i < courses_per_page:
     # re-find the course element to prevent stale element reference error
     course = driver.find_elements(By.TAG_NAME, 'osi-course-item')[i]
-    
+        
     # Click on course page
     actions = ActionChains(driver)
     actions.move_to_element(course).click().perform()
     print(f'opened course nummer {i + 1}')
-    
+        
     time.sleep(5)
 
     # Get the HTML source code of the last page
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-                
+                    
     course_name = soup.find('span', class_='font-heading-1 osi-white text text-md').get_text().strip()
     course_name_parts = course_name.split("\n")
     course_name = course_name_parts[0].strip()
@@ -61,25 +60,29 @@ while i < courses_per_page:
     content = soup.find_all('osi-body')[3].get_text().strip()
 
     course_dict = {'Course name': course_name, 
-                   'Course code': course_code,
-                   'Language': language,
-                   'Aims': aims,
-                   'Content': content}
-    
+                    'Course code': course_code,
+                    'Language': language,
+                    'Aims': aims,
+                    'Content': content}
+        
     with open('courses.csv', 'a', newline='', encoding='utf-8') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=course_dict.keys())
         if csvfile.tell() == 0:  # Check if the file is empty
             writer.writeheader()  # Write the header row
         writer.writerow(course_dict)
-            
+                
     print(f'Succesfully scraped course number {i + 1}, {course_name}')
-    
+        
     time.sleep(5)
-    
-	# Close course page
+        
+    # Close course page
     close = driver.find_element(By.CLASS_NAME, "bar-button-clear")
     actions = ActionChains(driver)
     actions.move_to_element(close).click().perform()
-    
+        
     time.sleep(5)
+
+    # Check how many courses are on this course list page
+    courses = driver.find_elements(By.TAG_NAME, 'osi-course-item')
+    courses_per_page = len(courses)
     i += 1
