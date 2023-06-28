672b6b94a4d7a497d2b33a2e9a289911b61d553d
KleinJonasUVT <123603524+KleinJonasUVT@users.noreply.github.com>
2023-06-28 23:36:42 +0000

    Updated database code with engine code

--- database.py
+++ database.py
@@ -1,2 +1,3 @@
-import sqlalchemy
-print(sqlalchemy.__version__)
+from sqlalchemy import create_engine, text
+
+engine = create_engine("mysql+pymysql://fru1fygudtvf4m501jgt:pscale_pw_eTrbwOGzfihxPHHTIP5K9LAHaW0GlAlifRdnnokm5gr@aws.connect.psdb.cloud/tiu_course_catalogue?charset=utf8mb4")
