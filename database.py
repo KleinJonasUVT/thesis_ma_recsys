e56431f29f21ebbf5c70f911b8af73f508cb175a
KleinJonasUVT <123603524+KleinJonasUVT@users.noreply.github.com>
2023-06-28 23:48:45 +0000

    updated database code

--- database.py
+++ database.py
@@ -1,3 +1,15 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine("mysql+pymysql://fru1fygudtvf4m501jgt:pscale_pw_eTrbwOGzfihxPHHTIP5K9LAHaW0GlAlifRdnnokm5gr@aws.connect.psdb.cloud/tiu_course_catalogue?charset=utf8mb4")
+db_connection_string = "mysql+pymysql://fru1fygudtvf4m501jgt:pscale_pw_eTrbwOGzfihxPHHTIP5K9LAHaW0GlAlifRdnnokm5gr@aws.connect.psdb.cloud/tiu_course_catalogue?charset=utf8mb4"
+
+engine = create_engine(
+  db_connection_string, 
+  connect_args ={
+    "ssl": {
+      "ssl_ca": "/etc/ssl/cert.pem"
+    }
+  } )
+
+with engine.connect() as conn:
+  result = conn.execute(text("select + from courses"))
+  print(results.all())
